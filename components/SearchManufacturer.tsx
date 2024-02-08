8b2dbbaca37c42628268494d5460946b09609bc5
adugbo aghogho <adugboaghogho@gmail.com>
2024-02-08 18:42:39 +0100

    css

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 import { Fragment, useState } from "react";
 import { Combobox, Transition } from "@headlessui/react";
 
-import { manufacturers } from "@constants";
-import { SearchManuFacturerProps } from "@types";
+import { manufacturers } from "@/constants";
+import { SearchManuFacturerProps } from "@/types";
 
 const SearchManufacturer = ({ manufacturer, setManuFacturer }: SearchManuFacturerProps) => {
   const [query, setQuery] = useState("");
@@ -12,12 +12,12 @@ const SearchManufacturer = ({ manufacturer, setManuFacturer }: SearchManuFacture
   const filteredManufacturers =
     query === ""
       ? manufacturers
-      : manufacturers.filter((item) =>
+      : manufacturers.filter((item) => (
           item
             .toLowerCase()
             .replace(/\s+/g, "")
             .includes(query.toLowerCase().replace(/\s+/g, ""))
-        );
+        ));
 
   return (
     <div className='search-manufacturer'>
