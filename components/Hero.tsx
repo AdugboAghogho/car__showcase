b990c23b70aa0c3b9d8eb8497d42044c459219c1
adugbo aghogho <adugboaghogho@gmail.com>
2024-02-08 11:06:05 +0100

    customButton

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>
-      Hero
+    <div className='hero'>
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+            Find, book, rent a car—quick and super easy!
+        </h1>
+
+        <p className="hero__subtitle">
+            Streamline your car rental experience with our effortless booking
+            process.
+        </p>
+      </div>
     </div>
   )
 }
