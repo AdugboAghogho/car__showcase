8b2fa02bd560f9de00c6ae3dcea50c9a379a41af
adugbo aghogho <adugboaghogho@gmail.com>
2024-02-10 14:30:22 +0100

    car images

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -41,10 +41,11 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                 leaveFrom='opacity-100 scale-100'
                 leaveTo='opacity-0 scale-95'
               >
-                <Dialog.Panel>
+                <Dialog.Panel className='relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white p-6 text-left shadow-xl transition-all flex flex-col gap-5'>
                   <button
                     type='button'
                     onClick={closeModal}
+                    className='absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full'
                   >
                     <Image 
                       src="/close.svg"
@@ -54,6 +55,28 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                       className='object-contain'
                     />
                   </button>
+
+                  <div className='flex-1 flex flex-col gap-3'>
+                    <div className='relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg'>
+                      <Image src='/hero.png' alt='car model' fill priority className='object-contain' />
+                    </div>
+
+                    <div className='flex gap-3'>
+                      <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
+                        <Image src={generateCarImageUrl(car, "29")} alt='car model' fill priority className='object-contain' />
+                      </div>
+
+                      <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
+                        <Image src={generateCarImageUrl(car, "33")} alt='car model' fill priority className='object-contain' />
+                      </div>
+
+                      <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
+                        <Image  src={generateCarImageUrl(car, "13")} alt='car model' fill priority className='object-contain' />
+                      </div>
+                    </div>
+                  </div>
+
+                  .
                 </Dialog.Panel>
               </Transition.Child>
             </div>
