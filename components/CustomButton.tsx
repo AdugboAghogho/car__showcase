72b1ea2c19f6dbb6c1e08223a63c9646be6221de
adugbo aghogho <adugboaghogho@gmail.com>
2024-02-10 13:49:30 +0100

    custombuttone righticon

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { CustomButtonProps } from '@/types'
 import React from 'react'
+import Image from 'next/image';
 
 const CustomButton = ({ title, containerStyles, handleClick, btnType, textStyles, rightIcon }: CustomButtonProps) => {
   return (
@@ -13,6 +14,16 @@ const CustomButton = ({ title, containerStyles, handleClick, btnType, textStyles
         <span className={`flex-1`}>
             {title}
         </span>
+        {rightIcon && (
+          <div className="relative w-6 h-6">
+            <Image 
+              src={rightIcon}
+              alt='right icon'
+              fill
+              className='object-contain'
+            />
+          </div>
+        )}
     </button>
   )
 }
