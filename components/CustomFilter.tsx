643b05a47d66b3e9653412d374a2bf21d4479f37
adugbo aghogho <adugboaghogho@gmail.com>
2024-02-11 06:09:03 +0100

    customfilter

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,9 +1,45 @@
+"use client";
 import React from 'react'
 
-const CustomFilter = () => {
+import { Fragment, useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { Listbox, Transition } from "@headlessui/react";
+
+import { CustomFilterProps } from "@/types";
+import { updateSearchParams } from "@/utils";
+
+const CustomFilter = ({ title, options }: CustomFilterProps) => {
+  const [selected,setSelected] = useState(options[0]);
+
   return (
-    <div>
-      CustomFilter
+    <div className='w-fit'>
+      <Listbox
+        value={selected}
+        onChange={(e) => setSelected(e)}
+      >
+        <div className="relative w-fit z-10">
+          <Listbox.Button className="custom-filter__btn">
+            <span className="block truncate">
+              {selected.title}
+            </span>
+
+            <Image
+              src='/chevron-up-down.svg'
+              alt='chevron up down'
+              width={20}
+              height={20} 
+              className='ml-4 object-contain'
+            />
+          </Listbox.Button>
+
+          <Transition
+            as={Fragment}
+          >
+            .
+          </Transition>
+        </div>
+      </Listbox>
     </div>
   )
 }
--- utils/index.ts
+++ utils/index.ts
@@ -52,7 +52,7 @@ export async function fetchCars(filters: FilterProps) {
 
   // Set the required headers for the API request
   const response = await fetch(
-    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
     {
       headers: headers,
     }
