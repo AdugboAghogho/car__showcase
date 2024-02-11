fc1d0f3973dc5e000db355dbdfb6ebb70c1b5359
adugbo aghogho <adugboaghogho@gmail.com>
2024-02-11 07:02:00 +0100

    customFilters done

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -10,13 +10,23 @@ import { CustomFilterProps } from "@/types";
 import { updateSearchParams } from "@/utils";
 
 const CustomFilter = ({ title, options }: CustomFilterProps) => {
+  const router = useRouter();
   const [selected,setSelected] = useState(options[0]);
+
+  const handleUpdateParams = (e: { title: string; value: string }) => {
+    const newPathName = updateSearchParams(title, e.value.toLowerCase());
+
+    router.push(newPathName);
+  };
 
   return (
     <div className='w-fit'>
       <Listbox
         value={selected}
-        onChange={(e) => setSelected(e)}
+        onChange={(e) => {
+          setSelected(e);
+          handleUpdateParams(e);
+        }}
       >
         <div className="relative w-fit z-10">
           <Listbox.Button className="custom-filter__btn">
--- utils/index.ts
+++ utils/index.ts
@@ -1,41 +1,33 @@
 import { CarProps, FilterProps } from "@/types";
 
 export const calculateCarRent = (city_mpg: number, year: number) => {
-  const basePricePerDay = 50; // Base rental price per day in dollars
-  const mileageFactor = 0.1; // Additional rate per mile driven
-  const ageFactor = 0.05; // Additional rate per year of vehicle age
+  const basePricePerDay = 50; 
+  const mileageFactor = 0.1; 
+  const ageFactor = 0.05; 
 
-  // Calculate additional rate based on mileage and age
   const mileageRate = city_mpg * mileageFactor;
   const ageRate = (new Date().getFullYear() - year) * ageFactor;
 
-  // Calculate total rental rate per day
   const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
 
   return rentalRatePerDay.toFixed(0);
 };
 
 export const updateSearchParams = (type: string, value: string) => {
-  // Get the current URL search params
   const searchParams = new URLSearchParams(window.location.search);
 
-  // Set the specified search parameter to the given value
   searchParams.set(type, value);
 
-  // Set the specified search parameter to the given value
   const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
 
   return newPathname;
 };
 
 export const deleteSearchParams = (type: string) => {
-  // Set the specified search parameter to the given value
   const newSearchParams = new URLSearchParams(window.location.search);
 
-  // Delete the specified search parameter
   newSearchParams.delete(type.toLocaleLowerCase());
 
-  // Construct the updated URL pathname with the deleted search parameter
   const newPathname = `${window.location.pathname}?${newSearchParams.toString()}`;
 
   return newPathname;
@@ -44,13 +36,11 @@ export const deleteSearchParams = (type: string) => {
 export async function fetchCars(filters: FilterProps) {
   const { manufacturer, year, model, limit, fuel } = filters;
 
-  // Set the required headers for the API request
   const headers: HeadersInit = {
     'X-RapidAPI-Key': '7863d4535amsh316a63b24ad3297p1c1a24jsnbc22829254b8',
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
   };
 
-  // Set the required headers for the API request
   const response = await fetch(
     `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
     {
