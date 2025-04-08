e3947a90d16357b7789e91f46545b66fc8a9e83b
adugbo aghogho <adugboaghogho@gmail.com>
2025-04-08 20:34:58 +0100

    showing now

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,60 +1,74 @@
 "use client";
-import React from 'react'
 
 import { Fragment, useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { Listbox, Transition } from "@headlessui/react";
 
-import { CustomFilterProps } from "@/types";
-import { updateSearchParams } from "@/utils";
+import { CustomFilterProps } from "../types";
+import { updateSearchParams } from "../utils";
 
-const CustomFilter = ({ title, options, setFilter }: CustomFilterProps) => {
-  const [selected,setSelected] = useState(options[0]);
+export default function CustomFilter({ title, options }: CustomFilterProps) {
+  const router = useRouter();
+  const [selected, setSelected] = useState(options[0]); // State for storing the selected option
 
-  
+  // update the URL search parameters and navigate to the new URL
+  const handleUpdateParams = (e: { title: string; value: string }) => {
+    const newPathName = updateSearchParams(title, e.value.toLowerCase());
+
+    router.push(newPathName);
+  };
 
   return (
-    <div className='w-fit'>
+    <div className="w-fit">
       <Listbox
         value={selected}
         onChange={(e) => {
-          setSelected(e);
-          setFilter(e.value);
+          setSelected(e); // Update the selected option in state
+          handleUpdateParams(e); // Update the URL search parameters and navigate to the new URL
         }}
       >
         <div className="relative w-fit z-10">
+          {/* Button for the listbox */}
           <Listbox.Button className="custom-filter__btn">
-            <span className="block truncate">
-              {selected.title}
-            </span>
-
+            <span className="block truncate">{selected.title}</span>
             <Image
-              src='/chevron-up-down.svg'
-              alt='chevron up down'
+              src="/chevron-up-down.svg"
               width={20}
-              height={20} 
-              className='ml-4 object-contain'
+              height={20}
+              className="ml-4 object-contain"
+              alt="chevron_up-down"
             />
           </Listbox.Button>
-
+          {/* Transition for displaying the options */}
           <Transition
-            as={Fragment}
-            leave='transiton ease-in duration-100'
-            leaveFrom='opacity-100'
-            leaveTo='opacity-0'
+            as={Fragment} // group multiple elements without introducing an additional DOM node i.e., <></>
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
           >
-            <Listbox.Options className='custom-filter__options'>
+            <Listbox.Options className="custom-filter__options">
+              {/* Map over the options and display them as listbox options */}
               {options.map((option) => (
                 <Listbox.Option
                   key={option.title}
+                  className={({ active }) =>
+                    `relative cursor-default select-none py-2 px-4 ${
+                      active ? "bg-primary-blue text-white" : "text-gray-900"
+                    }`
+                  }
                   value={option}
-                  className={({ active }) => `relative cursor-default select-none py-2 px-4 ${active ? 'bg-primary-blue text-white' : 'text-gray-900'}`}
                 >
                   {({ selected }) => (
-                    <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>
-                      {option.title}
-                    </span>
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? "font-medium" : "font-normal"
+                        }`}
+                      >
+                        {option.title}
+                      </span>
+                    </>
                   )}
                 </Listbox.Option>
               ))}
@@ -63,7 +77,5 @@ const CustomFilter = ({ title, options, setFilter }: CustomFilterProps) => {
         </div>
       </Listbox>
     </div>
-  )
+  );
 }
-
-export default CustomFilter
